3a41e51c6efcb5a2cfac5b8aa152030f145ece01
Petro Bes <beskolian@gmail.com>
2024-03-11 19:52:01 +0200

    clean the code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
@@ -10,9 +11,10 @@ async function bootstrap() {
   }));
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // don't allow send an additional properties to incoming request
+      whitelist: true,
     })
   );
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
