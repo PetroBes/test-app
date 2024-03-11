0a205afe7453bf468e8faed3f17a6e3655270793
Petro Bes <beskolian@gmail.com>
2024-03-11 13:50:29 +0200

    config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { User } from './models/user.entity';
+import { databaseConfig } from './config/dababase';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'auth-test',
-      password: 'test',
-      database: 'auth-db',
-      entities: [User],
-      synchronize: true, // only use in development environment
+    ConfigModule.forRoot({
+      load: [databaseConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('database'),
     }),
     UsersModule,
   ],
---
+++ src/config/dababase.ts
@@ -0,0 +1,18 @@
+export const databaseConfig = () => ({
+  database: {
+    type: 'postgres',
+    host: process.env.TYPEORM_HOST || 'postgres',
+    port: parseInt(process.env.TYPEORM_PORT || '5432', 10),
+    username: process.env.TYPEORM_USER || 'auth-test',
+    password: process.env.TYPEORM_PASSWORD || 'test',
+    database: process.env.TYPEORM_DATABASE || 'auth-db',
+    entities: ['**/*.entity.ts'],
+    migrations: ['migrations/**/*.ts'],
+    migrationsRun: process.env.TYPEORM_MIGRATIONS_RUN === 'true',
+    synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+    cli: {
+      entitiesDir: 'src',
+      migrationsDir: 'migrations',
+    },
+  }
+});
