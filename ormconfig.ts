14c74eb00db02fef10de5c975a1d0795e39d32b1
VasylKhoroshcho <vasyl.khoroshcho@rolique.io>
2024-03-12 12:09:36 +0200

    Fix

---
+++ ormconfig.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+
+export const connectionSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'auth-test',
+  password: 'test',
+  database: 'auth-db',
+  logging: false,
+  synchronize: false,
+  name: 'default',
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+});
